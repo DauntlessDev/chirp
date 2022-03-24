050c1d82b4d23e917dba9705b3ed59577fe7c0c6
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-03-24 18:41:57 +0800

    added basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './index.css'
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Home />
+    <><Router>
+    <div className='container'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Home />} />
+        <Route path='/register' element={<Home />} />
+      </Routes>
+    </div>
+  </Router>
     </>
   );
 }
