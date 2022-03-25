38aff52fd8dfd283b66e91f072026e230a8221c2
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-03-25 16:17:56 +0800

    fixed minor warnings

---
+++ client/src/components/Search/SidebarSearch.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+class SidebarSearch extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>SidebarSearch</div>
+    )
+  }
+}
+
+export default SidebarSearch
